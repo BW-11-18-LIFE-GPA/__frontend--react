4138ee1046db4a0b481bd9c8726bdd2bdcac8f96
jmjles <jmjles@hotmail.com>
2019-11-21 14:50:53 -0800

    Modal Complete

--- src/components/UserAttr.js
+++ src/components/UserAttr.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
-import { Typography as Font, Button, Modal, Input ,RadioGroup,Radio,TextField,FormGroup, FormControl, FormLabel, FormControlLabel} from "@material-ui/core";
+import {
+  Typography as Font,
+  Button,
+  Modal,
+  RadioGroup,
+  Radio,
+  TextField,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Container
+} from "@material-ui/core";
 
 class UserAttr extends Component {
   state = {
@@ -18,25 +30,91 @@ class UserAttr extends Component {
         <Font variant="body1">Gender: {gender}</Font>
         <Font variant="body1">Age: {age}</Font>
         <Font variant="body1">From: {nationality}</Font>
-        <Button variant="contained" color="primary" onClick={this.open}>
-          Edit Details
-        </Button>
-        <Modal open={this.state.open} onClose={this.close}>
-            <FormControl component= 'form'>
-                <TextField rows='3' variant='outlined' margin='normal' multiline label='About Me' required/>
-                <RadioGroup row>
-                    <FormLabel component='legend'>Gender</FormLabel>
-                    <FormControlLabel value='male' control={<Radio/>} label='Male' labelPlacement='start'/>
-                    <FormControlLabel value='female' control={<Radio/>} label='Female' labelPlacement='start'/>
-                    <FormControlLabel value='other' control={<Radio/>} label='Other' labelPlacement='start'/>
-                </RadioGroup>
-                    <TextField label='Age' type='number' required/>
-                    <TextField label='Where are you from?' type='text' required/>
-                    <Button type='submit' value='submit' name='submit' variant='outlined' color='primary'>Submit</Button>
-            </FormControl>
+        <Container align="center">
+          <Button variant="contained" color="primary" onClick={this.open}>
+            <Font variant="button">Edit Details</Font>
+          </Button>
+        </Container>
+
+        <Modal open={this.state.open} style={style.modal}>
+          <FormControl
+            component="form"
+            onSubmit={(this.props.handleSubmit, this.close)}
+            style={style.form}
+          >
+            <TextField
+              rows="3"
+              variant="outlined"
+              margin="normal"
+              multiline
+              label="About Me"
+              required
+              onChange={e => this.props.handleChange(e)}
+              name="bio"
+            />
+            <RadioGroup
+              row
+              onChange={this.props.handleChange}
+              defaultValue="other"
+              name="gender"
+            >
+              <FormLabel component="legend">Gender</FormLabel>
+              <FormControlLabel
+                value="male"
+                control={<Radio />}
+                label="Male"
+                labelPlacement="start"
+              />
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+                labelPlacement="start"
+              />
+              <FormControlLabel
+                value="other"
+                control={<Radio />}
+                label="Other"
+                labelPlacement="start"
+              />
+            </RadioGroup>
+            <TextField
+              label="Age"
+              type="number"
+              required
+              onChange={this.props.handleChange}
+              name="age"
+            />
+            <TextField
+              label="Where are you from?"
+              type="text"
+              required
+              onChange={this.props.handleChange}
+              name="nationality"
+            />
+            <Button
+              type="submit"
+              value="submit"
+              name="submit"
+              variant="outlined"
+              color="primary"
+            >
+              Submit
+            </Button>
+          </FormControl>
         </Modal>
       </>
     );
   }
 }
+const style = {
+  form: {
+    backgroundColor: "white",
+    padding: "15px"
+  },
+  modal: {
+    textAlign: "center",
+    marginTop: "60px"
+  }
+};
 export default UserAttr;
